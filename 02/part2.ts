14f176e623c86e5065033b9042e260a0570d753b
Matthew Bridgeman <matthew.bridgeman@boats.com>
2024-12-05 13:30:18 +0000

    Part2 work

---
+++ 02/part2.ts
@@ -0,0 +1,68 @@
+//The levels are either all increasing or all decreasing.
+//Any two adjacent levels differ by at least one and at most three.
+
+import { getReports } from "./fileHelper.ts";
+
+type SafetyReport = {
+  safe: boolean;
+  unsafeStep: number;
+}
+
+const evaluateSafety = (report: number[]): SafetyReport => {
+  let increasing = false;
+  let decreasing = false;
+  let safe = true;
+  let unsafeStep = -1;
+  for(let i = 0; i < report.length - 1; i++) {
+    const level1 = report[i];
+    const level2 = report[i+1];
+    const difference = level2 - level1;
+    const normalisedDifference = difference < 0 ? difference * -1 : difference;
+    const currentIncreasing = difference > 0;
+    const currentDecreasing = difference < 0;
+    const stillIncreasing = (!increasing || currentIncreasing === increasing) && !decreasing;
+    const stillDecreasing = (!decreasing || currentDecreasing === decreasing) && !increasing;
+    increasing = currentIncreasing;
+    decreasing = currentDecreasing;
+    const adjacentLevelsAreSafe = normalisedDifference <= 3 && normalisedDifference >= 1;
+    const stepIsSafe = (stillIncreasing || stillDecreasing) && adjacentLevelsAreSafe;
+    if(!stepIsSafe) {
+      safe = false;
+      unsafeStep = i;
+      break
+    }
+  }
+  return {
+    safe,
+    unsafeStep
+  }
+};
+
+const removeUnsafeStep = (report: number[], unsafeStep: number): number[] => {
+  const newReport = report.filter((_, index) => index !== unsafeStep);
+  return newReport;
+}
+
+const safeReports = async (): Promise<number> => {
+    const reports = await getReports();
+    const safeReportList = reports.map(report => {
+      const { safe, unsafeStep } = evaluateSafety(report);
+      if(!safe) {
+        const newReport = removeUnsafeStep(report, unsafeStep);
+        const { safe: newSafe } = evaluateSafety(newReport);
+        return newSafe ? 1 : 0;
+      }
+      return safe ? 1 : 0;
+    });
+    const sum = safeReportList.reduce((a, b) => a + b, 0);
+    return sum;
+};
+
+const main = async () => {
+  const safe = await safeReports();
+  console.log({safe});
+};
+
+if (import.meta.main) {
+  main();
+}
