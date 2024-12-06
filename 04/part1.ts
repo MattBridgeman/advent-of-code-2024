52d79ff00223b0fe598163f1b23562d37ddceee3
Matthew Bridgeman <matt_bridgeman@hotmail.co.uk>
2024-12-06 20:38:11 +0000

    Day 4 - Test coverage

--- 04/part1.ts
+++ 04/part1.ts
@@ -1,4 +1,29 @@
-export const executeWordsearch = (word: string) => (wordsearch: string) => {
+export const stringToRows = (s: string) => {
+  const rows = s.split("\n");
+  return rows;
+};
+
+export const reverseWord = (s: string) => {
+  return s.split("").reverse().join("");
+};
+
+export const wordIsInString = (word: string, s: string) => {
+  let index = s.indexOf(word);
+  return index;
+};
+
+export const searchHorizonal = (word: string) => (wordsearch: string[]): number => {
+  let reversedWord = reverseWord(word);
+  let occurencesArray = wordsearch.map(s => {
+    let foundWordIndex = wordIsInString(word, s);
+    let foundReverseWordIndex = wordIsInString(reversedWord, s);
+    return (foundWordIndex > -1 ? 1 : 0) + (foundReverseWordIndex > -1 ? 1 : 0);
+  });
+  let occurences = occurencesArray.reduce((a, b) => a + b, 0);
+  return occurences;
+};
+
+export const searchForWordInstances = (word: string) => (wordsearch: string) => {
   return 0;
 };
 
--- 04/part1_test.ts
+++ 04/part1_test.ts
@@ -1,13 +1,24 @@
 import { assertEquals } from "@std/assert";
-import { executeWordsearch } from "./part1.ts";
+import { searchForWordInstances, searchHorizonal, stringToRows } from "./part1.ts";
 
 export const getFixture = async (name: string) => {
   const fixture = await Deno.readTextFile(`./fixtures/${name}.txt`);
   return fixture;
 };
 
+//horizontal
+//vertical
+//diagonal
+//written backwards
+
+Deno.test("Wordsearch finds 5 xmas horizontally", async () => {
+  const wordsearch = await getFixture("input");
+  const numberOfInstances = searchHorizonal("XMAS")(stringToRows(wordsearch));
+  assertEquals(numberOfInstances, 5);
+});
+
 Deno.test("Wordsearch finds 18 xmas", async () => {
-  const wordsearch = await getFixture("input_with_18");
-  const numberOfInstances = executeWordsearch("XMAS")(wordsearch);
+  const wordsearch = await getFixture("input");
+  const numberOfInstances = searchForWordInstances("XMAS")(wordsearch);
   assertEquals(numberOfInstances, 18);
 });
