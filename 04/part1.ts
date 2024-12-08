81eb6b07c44531ef6c798198efc28c9b8a174a4f
Matthew Bridgeman <matt_bridgeman@hotmail.co.uk>
2024-12-08 20:59:32 +0000

    Fix part1

--- 04/part1.ts
+++ 04/part1.ts
@@ -61,9 +61,58 @@ export const rotateArray90Degrees = (wordsearch: Array<Array<string>>): Array<Ar
 //84
 //95
 //0
+
+//123
+//456
+//789
+
+//1
+//42
+//753
+//86
+//9
+
+// 1
+// 1
+
 export const rotateArray45Degrees = (wordsearch: Array<Array<string>>): Array<Array<string>> => {
   let firstRow = wordsearch[0];
+  const width = firstRow.length;
+  const height = wordsearch.length;
+  const maxDiagonal = Math.min(width, height);
+  const maxRotatedRows = width + height - 1;
   let rotatedArray: Array<Array<string>> = [];
+  // rotatedArray.fill([], 0, maxRotatedRows);
+  // for(let x = 0; x < width; x++) {
+  //   for(let y = 0; y < height; y++) {
+  //     const flippedX = 0;
+  //     const flippedY = 0;
+
+  //   }
+  // }
+  for(let y = 0; y < maxRotatedRows; y++) {
+    rotatedArray.push([]);
+    const diffY = (height - 1) - y;
+    // const overflowY = Math.min(0, diffY);
+    const endOverflowX = maxRotatedRows - y;
+    const overflowX = Math.min(Math.min(y + 1, maxDiagonal), endOverflowX);
+    for(let x = 0; x < overflowX; x++) {
+      rotatedArray[y].push('0');
+      // const rotatedX = x - y;
+      // const rotatedY = normalizedY;
+      // if(rotatedX < 0 || rotatedX > width - 1) {
+      //   continue;
+      // }
+      // if(!wordsearch[rotatedY]) {
+      //   throw new Error(`wordsearch rotatedY: ${rotatedY} doesn't exist`)
+      // }
+
+      // if(!wordsearch[rotatedY][rotatedX]) {
+      //   throw new Error(`wordsearch rotatedX: ${rotatedX} doesn't exist`)
+      // }
+      // rotatedArray[y].push(wordsearch[rotatedY][rotatedX]);
+    }
+  }
   return rotatedArray;
 };
 
@@ -79,6 +128,10 @@ export const searchForWordInstances = (word: string) => (wordsearch: string) => 
 };
 
 const main = async () => {
+  console.log(rotateArray45Degrees([
+    ["1","2","3","4","5"],
+    ["6","7","8","9","0"]
+  ]));
 };
 
 if (import.meta.main) {
